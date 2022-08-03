43565419a3c8cf5a24f60d6c701eb56ea3e805a8
Supriya Bauddh <73016178+SupriyaBauddh@users.noreply.github.com>
2022-08-03 13:32:49 +0530

    Delete tokenize directory

--- tokenize/src/main/java/com/nlp/tokenize/controller/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.nlp.tokenize.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<user,Long>{
-
-}
--- tokenize/src/main/java/com/nlp/tokenize/controller/RepositoryImpl.java
+++
@@ -1,5 +0,0 @@
-package com.nlp.tokenize.controller;
-
-public class RepositoryImpl {
-
-}
